a30715606ff4f179ddf6f700e1d01a62dbc8a38d
Hemant Bhatia (Nokia) <hemant.bhatia@nokia.com>
2023-09-03 14:17:32 +0530

    updated exercise detail page

--- src/pages/ExerciseDetailPage.js
+++ src/pages/ExerciseDetailPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { json, useParams} from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 import Detail from "../components/exercise-detail/Detail";
 import ExerciseVideos from "../components/exercise-detail/ExerciseVideos";
 import SimilarExercises from "../components/exercise-detail/SimilarExercises";
@@ -7,78 +7,140 @@ import { Box } from "@mui/material";
 import { exerciseOptions, fetchData, youtubeOptions } from "../utils/FetchData";
 import Loader from "../components/reusable/Loader";
 import Footer from "../assets/Footer";
+import PageContent from "../components/error/PageContent";
 
 function ExerciseDetailPage() {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState([]);
   const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
   const [equipmentExercises, setEquipmentExercises] = useState([]);
+  const [hasError, setHasError] = useState(false);
   // const allExercises = useLoaderData();
   const [isLoading, setIsLoading] = useState(false);
 
   const { exerciseId } = useParams();
 
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   window.scrollTo({ top: 0, behavior: "smooth" });
+
+  //   const fetchExerciseData = async () => {
+  //     const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+  //     const youtubeSearchUrl =
+  //       "https://youtube-search-and-download.p.rapidapi.com";
+
+  //     try {
+  //       const exerciseDetailData = await fetchData(
+  //         `${exerciseDbUrl}/exercises/exercise/${exerciseId}`,
+  //         exerciseOptions
+  //       );
+  //       // const exerciseDetailData = allExercises.find(
+  //       //   (exercise) => exercise.id === exerciseId
+  //       // );
+  //       setExerciseDetail(exerciseDetailData);
+  //       const exerciseVideoData = await fetchData(
+  //         `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
+  //         youtubeOptions
+  //       );
+  //       setExerciseVideos(exerciseVideoData.contents);
+
+  //       const targetMuscleExerciseData = await fetchData(
+  //         `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+  //         exerciseOptions
+  //       );
+
+  //       const equipmentExerciseData = await fetchData(
+  //         `${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
+  //         exerciseOptions
+  //       );
+
+  //       // const targetMuscleExerciseData = allExercises.filter(
+  //       //   (exercise) => exercise.target === exerciseDetailData.target
+  //       // );
+
+  //       // const equipmentExerciseData = allExercises.filter(
+  //       //   (exercise) => exercise.equipment === exerciseDetailData.equipment
+  //       // );
+
+  //       setIsLoading(false);
+  //       setTargetMuscleExercises(targetMuscleExerciseData);
+  //       setEquipmentExercises(equipmentExerciseData);
+  //     } catch (error) {
+  //       throw json({ message: "Could not fetch exercises" }, { status: "500" });
+  //     }
+  //   };
+
+  //   fetchExerciseData();
+  // }, [exerciseId]);
+
   useEffect(() => {
     setIsLoading(true);
+    setHasError(false);
     window.scrollTo({ top: 0, behavior: "smooth" });
 
-    const fetchExerciseData = async () => {
-      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+    const fetchAllData = async () => {
       const youtubeSearchUrl =
         "https://youtube-search-and-download.p.rapidapi.com";
 
-      try {
-        const exerciseDetailData = await fetchData(
-          `${exerciseDbUrl}/exercises/exercise/${exerciseId}`,
+      const fetchExerciseData = async () => {
+        return fetch(
+          "https://exercisedb.p.rapidapi.com/exercises",
           exerciseOptions
-        );
-        // const exerciseDetailData = allExercises.find(
-        //   (exercise) => exercise.id === exerciseId
-        // );
+        )
+          .then((response) => response.json())
+          .then((allExercises) => allExercises)
+          .catch((error) => error);
+      };
+
+      const fetchExerciseDetailData = (allExercises) => {
+        return new Promise((resolve, reject) => {
+          const exerciseDetailData = allExercises.find(
+            (exercise) => exercise.id === exerciseId
+          );
+          if (exerciseDetailData) {
+            resolve(exerciseDetailData);
+          } else {
+            reject("No exercise found with this Id.");
+          }
+        });
+      };
+
+      try {
+        const allExercises = await fetchExerciseData();
+        const exerciseDetailData = await fetchExerciseDetailData(allExercises);
         setExerciseDetail(exerciseDetailData);
+
         const exerciseVideoData = await fetchData(
           `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
           youtubeOptions
         );
         setExerciseVideos(exerciseVideoData.contents);
-
-        const targetMuscleExerciseData = await fetchData(
-          `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
-          exerciseOptions
-        );
-
-        const equipmentExerciseData = await fetchData(
-          `${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
-          exerciseOptions
-        );
-
-        // const targetMuscleExerciseData = allExercises.filter(
-        //   (exercise) => exercise.target === exerciseDetailData.target
-        // );
-
-        // const equipmentExerciseData = allExercises.filter(
-        //   (exercise) => exercise.equipment === exerciseDetailData.equipment
-        // );
-
         setIsLoading(false);
+
+        const targetMuscleExerciseData = allExercises.filter(
+          (exercise) => exercise.target === exerciseDetailData.target
+        );
         setTargetMuscleExercises(targetMuscleExerciseData);
+
+        const equipmentExerciseData = allExercises.filter(
+          (exercise) => exercise.equipment === exerciseDetailData.equipment
+        );
         setEquipmentExercises(equipmentExerciseData);
       } catch (error) {
-        throw json({ message: "Could not fetch exercises" }, { status: "500" });
+        setIsLoading(false);
+        setHasError(true);
+        console.log(error);
       }
     };
-
-    fetchExerciseData();
+    fetchAllData();
   }, [exerciseId]);
 
-  useEffect(() => {}, [exerciseId]);
 
-  if (!exerciseDetail) return <div>No Data</div>;
   return (
     <Box>
-      {isLoading ? (
-        <Loader />
-      ) : (
+      {isLoading && !hasError && <Loader />}
+      {hasError && <PageContent title="Not Found!" message="Could not find exercise"/>}
+      {!isLoading && !hasError && (
         <React.Fragment>
           <Detail exerciseDetail={exerciseDetail} />
           <ExerciseVideos
